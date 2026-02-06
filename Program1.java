b61b0fc04610fa128575ece0eb08c91f583d3b5a
Rajani14-cmd <dubeyrajani2704@gmail.com>
2026-02-06 12:09:42 +0530

    Initial Java code for DevOps

---
+++ Program1.java
@@ -0,0 +1,10 @@
+class DevOpsJenkinsDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to DevOps");
+        System.out.println("This Java program is executed using Jenkins");
+        System.out.println("Jenkins helps in automating build and deployment");
+        System.out.println("Continuous Integration makes development faster");
+    }
+}
